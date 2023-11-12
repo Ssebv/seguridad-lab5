c19be3b6def6050a5d9adb6da69a5212ae3908e4
Ssebv <sallendec@uft.edu>
2023-11-12 01:19:50 -0300

    Update Readme, funcion mod go

---
+++ README.md
@@ -0,0 +1,50 @@
+# Cliente-Servidor Diffie-Hellman con DES
+
+Se implementación un sistema cliente-servidor que utiliza el intercambio de claves Diffie-Hellman para establecer una clave compartida y cifra/descifra mensajes usando el algoritmo de cifrado DES.
+
+## Estructura 
+
+- **server.py**: Implementa el servidor en Python.
+- **client.go**: Implementa el cliente en Go.
+- **mensajerecibido.txt**: Archivo donde se guarda el mensaje descifrado.
+
+### Instrucciones de Uso
+
+1. **Clonar el Repositorio:**
+
+2. **Instalar Dependencias Python:**
+    - Asegúrate de tener instalado `python`
+    ```bash
+        pip install -r requirements.txt
+    ```
+
+3. **Ejecutar el Servidor:**
+    ```bash
+    python3 server.py
+    ```
+
+4. **Ejecutar el Cliente:**
+    - Asegúrate de tener Go instalado.
+    ```bash
+    go run client.go
+    ```
+
+5. **Verificar Resultados:**
+    - El mensaje descifrado se guardará en el archivo `mensajerecibido.txt`.
+
+### Notas Importantes
+
+- Asegúrate de tener Python y Go instalados en tu sistema.
+- La comunicación entre el cliente y el servidor se realiza a través del puerto 65000, se puede cambiar en el código.
+- Se utiliza Diffie-Hellman para el intercambio de claves y DES para el cifrado de mensajes.
+
+### Requisitos del Sistema
+
+- Python 3.x
+- Go (Golang)
+- Pipenv (opcional)
+
+### Integrantes
+
+- Sebastian Allende
+- Gianfranco Astorga
--- src/client.go
+++ src/client.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"crypto/dsa"
-	"crypto/rand"
-	"fmt"
 	"math/big"
-	"net"
 )
 
-func modExp(base, exp, mod *big.Int) *big.Int {
+func mod(base, exp, mod *big.Int) *big.Int {
 	result := new(big.Int).SetInt64(1)
-	base = new(big.Int).Mod(base, mod)
 	zero := new(big.Int)
+
+	base = new(big.Int).Mod(base, mod)
 
 	for exp.Cmp(zero) > 0 {
 		if exp.Bit(0) == 1 {
@@ -25,78 +22,5 @@ func modExp(base, exp, mod *big.Int) *big.Int {
 }
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:65002")
-	if err != nil {
-		fmt.Println("Error connecting:", err)
-		return
-	}
-	defer conn.Close()
 
-	fmt.Println("Connected to server")
-
-	// Recibir parámetros públicos
-	bufferP := make([]byte, 1024)
-	n, err := conn.Read(bufferP)
-	if err != nil {
-		fmt.Println("Error receiving P:", err)
-		return
-	}
-
-	p := new(big.Int).SetBytes(bufferP)
-	fmt.Println("Received P:", p)
-
-	bufferG := make([]byte, 1024)
-	n, err = conn.Read(bufferG)
-	if err != nil {
-		fmt.Println("Error receiving G:", err)
-		return
-	}
-	g := new(big.Int).SetBytes(bufferG)
-	fmt.Println("Received G:", g)
-
-	// Generar claves
-	privateKey := new(dsa.PrivateKey)
-	privateKey.PublicKey.Parameters = dsa.Parameters{
-		P: p,
-		Q: new(big.Int).SetInt64(2),
-		G: g,
-	}
-	err = dsa.GenerateKey(privateKey, rand.Reader)
-	if err != nil {
-		fmt.Println("Error generating DSA keys:", err)
-		return
-	}
-
-	// Enviar clave pública
-	fmt.Println("Sending public key:", privateKey.PublicKey.Y)
-	conn.Write(privateKey.PublicKey.Y.Bytes())
-
-	// Recibir clave pública
-	serverPublicKeyBytes := make([]byte, 2048)
-	n, err = conn.Read(serverPublicKeyBytes)
-	if err != nil {
-		fmt.Println("Error receiving server's public key:", err)
-		return
-	}
-	serverPublicKey := new(big.Int).SetBytes(serverPublicKeyBytes)
-	fmt.Println("Received server's public key:", serverPublicKey)
-
-	// Calcular clave compartida
-	sharedKey := modExp(serverPublicKey, privateKey.X, p)
-	fmt.Println("Shared Key:", sharedKey)
-
-	// Enviar mensaje
-	mensaje := "Hola, servidor!"
-	conn.Write([]byte(mensaje))
-	fmt.Println("Mensaje enviado al servidor:", mensaje)
-
-	// Recibir mensaje
-	buffer := make([]byte, 1024)
-	n, err = conn.Read(buffer)
-
-	if err != nil {
-		fmt.Println("Error receiving message:", err)
-		return
-	}
-	fmt.Println("Mensaje recibido del servidor:", string(buffer[:n]))
 }
